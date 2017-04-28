d7de3e3e0267ea13ad7d18dfeed0b818ad313e4a
eddie2k <jesus_eme_ge@hotmail.com>
2017-04-28 22:10:34 +0200

    Use constant for number of cars

--- web/webapp.rb
+++ web/webapp.rb
@@ -5,6 +5,8 @@ require_relative 'errors'
 
 APP_BIND_ADDRESS = '0.0.0.0'
 
+NUMBER_OF_CARS = 10
+
 DBHOST = 'db'
 DBPORT= 5432
 DBNAME = 'ally_cars'
@@ -25,13 +27,13 @@ get '/cars' do
 
 	begin
 	    db = PostgresDb.new(DBHOST, DBPORT, DBNAME, DBUSER, DBPASSWORD)
-	    json = db.getNClosestCars(paramParser.getLatitude(), paramParser.getLongitude(), 10)
+	    json = db.getNClosestCars(paramParser.getLatitude(), paramParser.getLongitude(), NUMBER_OF_CARS)
 	    status 200
 	    headers = {'Content-Type' => 'application/json'}
 	    body json
     rescue IOError => e
 		    halt 503, {'Content-Type' => 'text/plain'}, "Error connecting to the database"
     rescue NotEnoughCarsError => e
-		    halt 400, {'Content-Type' => 'text/plain'}, "Sorry, we couldn't find 10 suitable cars for your location"
+		    halt 400, {'Content-Type' => 'text/plain'}, "Sorry, we couldn't find "+ NUMBER_OF_CARS + " suitable cars for your location"
     end
 end
